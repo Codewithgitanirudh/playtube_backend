186ce89b756e10138d9f05cb3aee2e5ec26ee8a6
anirudh <anirudhjangid)@gmail.com>
2025-07-05 19:29:09 +0530

    Implement tweet management functionality: added create, read, update, and delete operations for tweets with appropriate validation and error handling.

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,76 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  const { content } = req.body;
+
+  if (!content || content.length === 0) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const tweet = await Tweet.create({ content, owner: req.user._id });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, tweet, "Tweet created successfully"));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid tweet id");
+  }
+
+  const tweets = await Tweet.find({ owner: userId }).populate({
+    path: "owner",
+    select: "-password -refreshToken -__v",
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  const { tweetId } = req.params;
+  const { content } = req.body;
+  console.log(content, tweetId, "habibi");
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "invalid tweet id");
+  }
+
+  if (!content) {
+    throw new ApiError(400, "content is required");
+  }
+
+  const updatedTweets = await Tweet.findByIdAndUpdate(
+    tweetId,
+    { content: content },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweets, "tweet updated successfully"));
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+  const { tweetId } = req.params;
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "invalid tweet id");
+  }
+
+  const deleteTweet = await Tweet.deleteOne({ _id: tweetId });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "tweet deleted successfully"));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
